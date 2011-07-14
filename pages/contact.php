33ee1fc874275c93d4103f39b37ed8da445aeb97
Sam Ferry <sam@atheme.org>
2011-07-14 01:59:32 -0500

    addition of #iris to contact page

--- pages/contact.php
+++ pages/contact.php
@@ -7,6 +7,7 @@ At the moment, IRC is our primary form of contact. Our IRC server is located at 
 	<li>For atheme and atheme-web, <a href="irc://irc.atheme.org/atheme"><span class="italics bold">#atheme</span></a></li>
 	<li>For audacious2, <a href="irc://irc.atheme.org/audacious"><span class="italics bold">#audacious</span></a></li>
 	<li>For charybdis, <a href="irc://irc.atheme.org/charybdis"><span class="italics bold">#charybdis</span></a></li>
+	<li>For iris, <a href="irc://irc.atheme.org/iris"><span class="italics bold">#iris</span></a></li>
 </ul>
 Issues with the website? Email <span class="italics bold">Sam Ferry</span> <strong><<a href="mailto:sam@atheme.org">sam@atheme.org</a>></strong><br />
 Issues with anything else? Email <span class="italics bold">William Pitcock</span> <strong><<a href="mailto:nenolod@atheme.org">nenolod@atheme.org</a>></strong>
