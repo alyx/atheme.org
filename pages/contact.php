1a0c424901dea03f6813069b5a0b7fb8f434e4b3
Sam Ferry <sam@atheme.org>
2011-07-14 02:10:00 -0500

    pages/contact: reauthoring

--- pages/contact.php
+++ pages/contact.php
@@ -1,14 +1,21 @@
 <div class="heading">Talk to Us</div>
 
 <p>
-At the moment, IRC is our primary form of contact. Our IRC server is located at <a href="irc://irc.atheme.org"><span class="italics bold">irc.atheme.org</span></a>.
+Currently, IRC is the best way to get in touch with the individuals behind Atheme. You can find us on the <a href="http://www.staticbox.net">StaticBox network</a> via <a href="irc://irc.atheme.org"><strong>irc.atheme.org</strong></a>.
+We have multiple channels for our various projects, however <a href="irc://irc.atheme.org/atheme-project"><span class="italics bold">#atheme-project</span></a> is used as a general discussion channel for the project. Below, a few other
+project channels are listed. In parenthesis, we've listed a few of the developers' IRC usernames.
 <ul>
-	<li>For general project-related topics, <a href="irc://irc.atheme.org/atheme-project"><span class="italics bold">#atheme-project</span></a></li>
-	<li>For atheme and atheme-web, <a href="irc://irc.atheme.org/atheme"><span class="italics bold">#atheme</span></a></li>
-	<li>For audacious2, <a href="irc://irc.atheme.org/audacious"><span class="italics bold">#audacious</span></a></li>
-	<li>For charybdis, <a href="irc://irc.atheme.org/charybdis"><span class="italics bold">#charybdis</span></a></li>
-	<li>For iris, <a href="irc://irc.atheme.org/iris"><span class="italics bold">#iris</span></a></li>
+<li><a href="irc://irc.atheme.org/atheme"><span class="italics bold">#atheme</span></a> &mdash; Atheme Services (jilles, nenolod, jdhore, Aerdan)</li>
+<li><a href="irc://irc.atheme.org/charybdis"><span class="italics bold">#charybdis</span></a> &mdash; charybdis IRC daemon (jilles, nenolod)</li>
+<li><a href="irc://irc.atheme.org/audacious"><span class="italics bold">#audacious</span></a> &mdash; Audacious Media Player (nenolod)</li>
+<li><a href="irc://irc.atheme.org/iris"><span class="italics bold">#iris</span></a> &mdash; Iris Web IRC Client for Atheme</li>
 </ul>
-Issues with the website? Email <span class="italics bold">Sam Ferry</span> <strong><<a href="mailto:sam@atheme.org">sam@atheme.org</a>></strong><br />
-Issues with anything else? Email <span class="italics bold">William Pitcock</span> <strong><<a href="mailto:nenolod@atheme.org">nenolod@atheme.org</a>></strong>
+</p>
+
+<p>
+As far as contact via email, we can be reached at the following:
+<ul>
+	<li>Issues with the website? Email <span class="italics bold">Sam Ferry</span> <strong><<a href="mailto:sam@atheme.org">sam@atheme.org</a>></strong></li>
+	<li>Issues with anything else? Email <span class="italics bold">William Pitcock</span> <strong><<a href="mailto:nenolod@atheme.org">nenolod@atheme.org</a>></strong></li>
+</ul>
 </p>
