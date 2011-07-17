8c2f98d5c8d884dd5cc75131eaf4766eb47741b6
Sam Ferry <sam@atheme.org>
2011-07-17 00:40:42 -0500

    pages/contact: minor changes

--- pages/contact.php
+++ pages/contact.php
@@ -2,9 +2,9 @@
 
 <p>
 Currently, IRC is the best way to get in touch with the individuals behind Atheme. You can find us on the <a href="http://www.staticbox.net">StaticBox network</a> via <a href="irc://irc.atheme.org"><strong>irc.atheme.org</strong></a>.
-We have multiple channels for our various projects, however <a href="irc://irc.atheme.org/atheme-project"><span class="italics bold">#atheme-project</span></a> is used as a general discussion channel for the project. Below, a few other
-project channels are listed. In parenthesis, we've listed a few of the developers' IRC usernames. If you'd prefer not to use a desktop IRC client, the web client is linked in the navigation bar above.
+We have multiple channels for our various projects. Below, they are listed. In parenthesis, we've listed a few of the developers' IRC usernames. If you'd prefer not to use a desktop IRC client, the web client is linked in the navigation bar above.
 <ul>
+<li><a href="irc://irc.atheme.org/atheme-project"<span class="italics bold">#atheme-project</span></a> &mdash; Atheme Project (nenolod, jilles, jdhore, Aerdan, sam)</li>
 <li><a href="irc://irc.atheme.org/atheme"><span class="italics bold">#atheme</span></a> &mdash; Atheme Services (jilles, nenolod, jdhore, Aerdan)</li>
 <li><a href="irc://irc.atheme.org/charybdis"><span class="italics bold">#charybdis</span></a> &mdash; charybdis IRC daemon (jilles, nenolod)</li>
 <li><a href="irc://irc.atheme.org/audacious"><span class="italics bold">#audacious</span></a> &mdash; Audacious Media Player (nenolod, jlindgren)</li>
