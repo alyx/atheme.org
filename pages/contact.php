d0479e455c1a711fc424db9f3a20ebf40c8a2f98
Sam Ferry <sam@atheme.org>
2012-03-20 04:51:37 +0000

    Change of IRC links from irc:// to webchat

--- pages/contact.php
+++ pages/contact.php
@@ -4,11 +4,11 @@
 	Currently, IRC is the best way to get in touch with the individuals behind Atheme. You can find us on the <a href="http://www.staticbox.net">StaticBox network</a> via <a href="irc://irc.atheme.org"><strong>irc.atheme.org</strong></a>.
 	We have multiple channels for our various projects. Below, they are listed. In parenthesis, we've listed a few of the key individuals' and developers' usernames. If you'd prefer not to use a desktop IRC client, the web client is linked in the navigation bar above.
 	<ul>
-		<li><a href="irc://irc.atheme.org/atheme-project"<span class="italics bold">#atheme-project</span></a> &mdash; Atheme Project (nenolod, jilles, jdhore, Aerdan, sam)</li>
-		<li><a href="irc://irc.atheme.org/atheme"><span class="italics bold">#atheme</span></a> &mdash; Atheme Services (jilles, nenolod, jdhore, Aerdan)</li>
-		<li><a href="irc://irc.atheme.org/charybdis"><span class="italics bold">#charybdis</span></a> &mdash; charybdis IRC daemon (jilles, nenolod)</li>
-		<li><a href="irc://irc.atheme.org/audacious"><span class="italics bold">#audacious</span></a> &mdash; Audacious Media Player (nenolod, jlindgren)</li>
-		<li><a href="irc://irc.atheme.org/iris"><span class="italics bold">#iris</span></a> &mdash; Iris Web IRC Client for Atheme (Namegduf)</li>
+		<li><a href="http://webchat.staticbox.net/?channels=#atheme-project"<span class="italics bold">#atheme-project</span></a> &mdash; Atheme Project (nenolod, jilles, jdhore, Aerdan, sam)</li>
+		<li><a href="http://webchat.staticbox.net/?channels=#atheme"><span class="italics bold">#atheme</span></a> &mdash; Atheme Services (jilles, nenolod, jdhore, Aerdan)</li>
+		<li><a href="http://webchat.staticbox.net/?channels=#charybdis"><span class="italics bold">#charybdis</span></a> &mdash; charybdis IRC daemon (jilles, nenolod)</li>
+		<li><a href="http://webchat.staticbox.net/?channels=#audacious"><span class="italics bold">#audacious</span></a> &mdash; Audacious Media Player (nenolod, jlindgren)</li>
+		<li><a href="http://webchat.staticbox.net/?channels=#iris"><span class="italics bold">#iris</span></a> &mdash; Iris Web IRC Client for Atheme (Namegduf)</li>
 	</ul>
 </p>
 
