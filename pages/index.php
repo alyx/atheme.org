d0da45d624034757c8c6ed7163f8b4ae88fb5258
Sam Ferry <sam@atheme.org>
2012-03-20 05:03:06 +0000

    link updates, content changes, small functionality tweaks

--- pages/index.php
+++ pages/index.php
@@ -2,20 +2,20 @@
 <div class="subheading">We are Atheme, an open-source development collective. Questions or concerns in regard to the website? <a href="http://www.atheme.org/contact" title="Contact Us"><strong>Contact us</strong></a>.</div>
 
 <p>
-	<strong>Recently, Atheme has acquired the popular revision control tracking service, <a href="http://cia.vc">cia.vc</a></strong>
+	<strong>Recently, Atheme has acquired the popular revision control tracking service, <a href="http://cia.vc" title="CIA">cia.vc</a></strong>
 </p>
 <p>
 	As you can see, we don't have that much listed here on the home page, as there isn't that much to say.
-	Mainly, we develop stuff, so check out the projects page for more information on that.
-	Wiki has some information, the bug tracker is useful (not really). For the development
-	versions of the Atheme projects, check out the repository linked above.
+	Mainly, we develop stuff, so check out the <a href="http://www.atheme.org/projects" title="Projects">projects page</a> for more information.
+	Our <a href="http://confluence.atheme.org" title="Atheme Wiki">Wiki</a> also has useful information, as well as the <a href="http://jira.atheme.org" title="JIRA Bug Tracker">bug tracker</a>.
+	For many development versions of the Atheme projects, check out <a href="http://git.atheme.org" title="Git">our Git repository</a>.
 </p>
 <p>
-	If you have any suggestions on how to improve the website, contact our webmaster via email, or locate us on IRC at <a href="irc://irc.atheme.org/atheme-project"><span class="italics bold">irc.atheme.org</span></a>. 
+	If you have any suggestions on how to improve the website, contact our webmaster via email, or <a href="http://webchat.staticbox.net/?channels=#atheme-project" title="#atheme-project on StaticBox IRC"><span class="italics bold">locate us on IRC via the webchat or at irc.atheme.org</span></a>.
 	Alternatively, if you have an issue or a suggestion, you can open up an issue at <a href="http://jira.atheme.org/browse/WEB"><strong>at the bug tracker</strong></a> and it will be seen and taken care of promptly.
 </p>
 <p>
-	If you're interested in tracking the development of the project's website, which is now apparently a subproject, head over to <a href="http://git.atheme.org/atheme.org">Git</a>.
+	If you're interested in tracking the development of the project's website, there is plenty of information to be found on the <a href="http://git.atheme.org/atheme.org">Git repository</a> for Atheme.org.
 </p>
 <p>
 	Thanks.
