9ce6bc3ade08afd51f6686670f48075813bf146f
Alyx <alyx@malkier.net>
2012-05-17 19:17:35 -0500

    Update this to reflect mowgli-2 readme

--- pages/projects/mowgli.php
+++ pages/projects/mowgli.php
@@ -11,7 +11,7 @@
 	<li>mowgli_argstack: Safe serialization of valists.</li>
 	<li>mowgli_assert: Various assertion routines that can be used.</li>
 	<li>mowgli_bitvector: Bitmasks with an unlimited level of precision.</li>
-	<li>mowgli_dictionary: A keyword-backed definition hashtable class.</li>
+    <li>mowgli_patricia: A keyword-backed definition hashtable class.</li>
 	<li>mowgli_error_backtrace: Provide feedback to users on what caused the error they are recieving.</li>
 	<li>mowgli_exception: Assertions with user feedback.</li>
 	<li>mowgli_formatter: A simple token formatter which is sometimes useful.</li>
@@ -19,10 +19,8 @@
 	<li>mowgli_hash: A portable implementation of the FNV-1 hash.</li>
 	<li>mowgli_heap: An optimistic heap-based memory allocator.</li>
 	<li>mowgli_hook: A simple hooks API you can use for your application, which allows for hooks to provide both application data and user data.</li>
-	<li>mowgli_ioevent: Portable I/O completion ports for many OSes.</li>
 	<li>mowgli_list: A high performance linked lists implementation with O(1) scalability for most common operations.</li>
 	<li>mowgli_logger: An internal class for handling logging of exceptions.</li>
-	<li>mowgli_memorypool: A class which allows for memory pooling.</li>
 	<li>mowgli_module: A wrapper around dlopen(3) and dlsym(3).</li>
 	<li>mowgli_object: A simple class which provides reference counted pointers and polymorphism of structs.</li>
 	<li>mowgli_object_class: Classing and subclassing for objects.</li>
@@ -32,6 +30,10 @@
 	<li>mowgli_random: A high performance psuedo-random number generator.</li>
 	<li>mowgli_signal: A wrapper for sigaction(2).</li>
 	<li>mowgli_spinlock: Portable spinlocks.</li>
+    <li>mowgli_eventloop: A portable event loop implementation.</li>
+    <li>mowgli_vio: An abstraction layer for I/O.</li>
+    <li>mowgli_linebuf: A line-buffering implementation for clients.</li>
+    <li>mowgli_thread: Minimal thread abstraction.</li>
 	</ul>
 	</div>
 
